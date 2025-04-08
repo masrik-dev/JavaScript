8f58274f1a0352993e01687c5f3e9412ae45f0a2
masrik-dev <111740924+masrik-dev@users.noreply.github.com>
2025-04-08 23:55:34 +0600

    --update js (2nd)

--- JS2/js/main.js
+++ JS2/js/main.js
@@ -1,4 +1,14 @@
-console.log("Hello world!");
-console.log(typeof "MH");
-console.log(typeof 42);
-console.log(typeof true);
+// Strings
+const myVariable = "Mathematics";
+
+// The length property
+console.log(myVariable.length);
+
+// String Methods
+console.log(myVariable.charAt(6));
+
+console.log(myVariable.indexOf("Mat"));
+
+console.log(myVariable.slice(5, 8));
+
+console.log(myVariable.split(""));
