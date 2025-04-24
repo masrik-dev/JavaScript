f820b6efdb80e92b999431c4b3a47d5ee02f86cd
masrik-dev <111740924+masrik-dev@users.noreply.github.com>
2025-04-24 20:27:55 +0600

    --update js (if statements 1st)

--- JS2/js/main.js
+++ JS2/js/main.js
@@ -117,8 +117,45 @@ console.log("Marvik".charAt(Math.floor(Math.random() * anyName.length)));
 console.log("End");
 
 // Conditionals: If statements
+let customerIsBanned = false;
+let soup = "chicken noodle soup";
+let crackers = true;
+let reply;
+if (customerIsBanned) {
+    reply = "No soup for you!";
+} else if (soup && crackers) {
+    reply = `Here's your order of ${soup} and crackers.`;
+} else if (soup) {
+    reply = `Here's your order of ${soup}.`;
+} else {
+    reply = `Sorry, we're out of ${soup}`;
+}
+console.log(reply);
 
+// New example
+let testScore = 59;
+let collegeStudent = true;
+let grade;
 
+if (testScore >= 90) {
+    grade = "A";
+} else if (testScore >= 80) {
+    grade = "B";
+} else if (testScore >= 70) {
+    grade = "C";
+} else if (testScore >= 60) {
+    grade = "D";
+} else {
+    // Nested if statement
+    if (collegeStudent) {
+        grade = "U";
+    } else {
+        grade = "F";
+    }
+}
+console.log(grade);
+
+// Decision tree!
 
 
 
