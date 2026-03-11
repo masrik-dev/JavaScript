d3a756908fad4fa8945e5e8df0f8f0d7bdb10866
masrik-dev <111740924+masrik-dev@users.noreply.github.com>
2026-03-11 08:43:54 +0600

    --update Array upto reverse

--- JS2/js/test.js
+++ JS2/js/test.js
@@ -1,27 +1,67 @@
-var x = 1; // function scoped
-let y = 2; // block scoped
-const z = 3;
+// Arrays
+const myArray = [];
 
-console.log(`global: ${x}`);
-console.log(`global: ${y}`);
-console.log(`global: ${z}`);
+// add elements to an array 
+myArray[0] = "Myking";
+myArray[1] = 1001;
+myArray[2] = false;
 
-function myFunc() {
-    var x = 10;
-    const z = 5;
+//refer to an array
+console.log(myArray);
 
-    console.log(`function: ${x}`);
-    console.log(`function: ${y}`);
-    console.log(`function: ${z}`);
+// length property
+console.log(myArray.length);
 
-    {
-        var x = 11; // function scoped, not block scoped
-        const z = 6; // block scoped
+// last element in an array
+console.log(myArray[myArray.length - 1]);
 
-        console.log(`block: ${x}`);
-        console.log(`block: ${y}`);
-        console.log(`block: ${z}`);
-    }
-}
+console.log(myArray[1]);
 
-myFunc();
+// add more data to our array
+myArray.push("school");
+
+console.log(myArray);
+
+// remove something from our array
+const lastItem = myArray.pop();
+console.log(lastItem);
+
+// add and remove something from front
+const newLength = myArray.unshift(42);
+
+console.log(newLength);
+console.log(myArray);
+
+// remove
+const firstItem = myArray.shift();
+
+console.log(firstItem);
+console.log(myArray);
+
+// remove or replace something from the midel of the array by 'splice' methods
+
+// replace
+myArray.splice(1, 1, 42);
+console.log(myArray);
+
+// remove
+myArray.splice(1, 1);
+console.log(myArray);
+
+// not delete but incert a value or element in the midel of the array
+myArray.splice(1, 0, 1001);
+console.log(myArray);
+
+// Different type of methods
+const myArray_2 = ["A", "B", "C", "D", "E", "F"];
+
+const newArray = myArray_2.slice(2, 5);
+
+console.log(newArray);
+console.log(myArray_2);
+
+// reverse methods
+const newArray_2 = myArray_2.reverse();
+console.log(newArray_2); 
+
+// join methods 2:21:02
