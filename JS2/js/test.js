a019b0f8be78d33d15dc4491afa87ce217617a8b
masrik-dev <111740924+masrik-dev@users.noreply.github.com>
2026-03-19 23:39:12 +0600

    --update Three dimensional array

--- JS2/js/test.js
+++ JS2/js/test.js
@@ -103,3 +103,9 @@ const clothesDept = [clothesShelfA, clothesShelfB];
 
 console.log(equipDept[0][1]);
 console.log(clothesDept[1][0]);
+
+// Three dimensional array
+const sportsStore = [equipDept, clothesDept];
+console.log(sportsStore);
+console.log(sportsStore[0][0][1]);
+console.log(sportsStore[1][1][0]);
