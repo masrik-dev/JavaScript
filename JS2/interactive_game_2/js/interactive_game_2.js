8724fb4ee83a9ef2cfcb9d1375273e3ebe93175c
masrik-dev <111740924+masrik-dev@users.noreply.github.com>
2026-03-23 14:59:51 +0600

    --update interactive_game_2

--- JS2/interactive_game_2/js/interactive_game_2.js
+++ JS2/interactive_game_2/js/interactive_game_2.js
@@ -3,7 +3,7 @@ let playGame = confirm("Shall we play rock, paper, or scissors?");
 if (playGame) {
     //play
     while (playGame) {
-        const playerChoice = prompt("Please enter rock, paper, or scissorr:");
+        const playerChoice = prompt("Please enter rock, paper, or scissors:");
         if (playerChoice || playerChoice === "") {
             const playerOne = playerChoice.trim().toLowerCase();
             if (
@@ -20,7 +20,24 @@ if (playGame) {
                         ? "Tie game!"
                         : playerOne === "rock" && computer === "paper"
                         ? `playerOne: ${playerOne}\nComputer: ${computer} \nComputer Wins!`
+                        : playerOne === "paper" && computer === "scissors"
+                        ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer Wins!`
+                        : playerOne === "scissors" && computer === "rock"
+                        ? `playerOne: ${playerOne}\nComputer: ${computer} \nComputer wins!`
+                        : `playerOne: ${playerOne}\nComputer: ${computer} \nPlayer wins!`;
+                alert(result);
+                playGame = confirm("Play Again?");
+                if (!playGame) alert("Ok, thanks for playing.");
+                continue;
+            } else {
+                alert("You didn't enter rock, paper, or scissors.");
+                continue;
             }
+        } else {
+            alert("I guess you changed your mind. Maybe next time.");
+            break;
         }
     }
+} else {
+    alert("Ok, maybe next time.");
 }
