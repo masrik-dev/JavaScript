b5e4ebd108f6c09550e6137892036c19194ccd55
masrik-dev <111740924+masrik-dev@users.noreply.github.com>
2026-03-24 23:01:16 +0600

    --update object_1

--- JS2/js/main.js
+++ JS2/js/main.js
@@ -675,3 +675,27 @@ const sportsStore = [equipDept, clothesDept];
 console.log(sportsStore);
 console.log(sportsStore[0][0][1]);
 console.log(sportsStore[1][1][0]);
+
+// Objects
+// key-value pairs in curly braces
+const myObj = { name: "Dove" };
+console.log(myObj.name);
+
+const anotherObj = {
+    alive: true,
+    answer: 42,
+    hobbies: ["Eat", "Sleep", "Code"],
+    beverage: {
+        morning: "Coffee",
+        afternoor: "Iced Tea"
+    }
+};
+
+console.log(anotherObj.answer);
+console.log(anotherObj.hobbies[1]);
+console.log(anotherObj.beverage[0]); // won't work, it will be undefined
+
+console.log(anotherObj["alive"]);
+console.log(anotherObj["beverage"]); // for nested object
+
+console.log(anotherObj.beverage.morning); // for nested object
--- JS2/js/test.js
+++ JS2/js/test.js
@@ -1,111 +1,23 @@
-// Arrays
-const myArray = [];
+// Objects
+// key-value pairs in curly braces
+const myObj = { name: "Dove" };
+console.log(myObj.name);
 
-// add elements to an array 
-myArray[0] = "Myking";
-myArray[1] = 1001;
-myArray[2] = false;
+const anotherObj = {
+    alive: true,
+    answer: 42,
+    hobbies: ["Eat", "Sleep", "Code"],
+    beverage: {
+        morning: "Coffee",
+        afternoor: "Iced Tea"
+    }
+};
 
-//refer to an array
-console.log(myArray);
+console.log(anotherObj.answer);
+console.log(anotherObj.hobbies[1]);
+console.log(anotherObj.beverage[0]); // won't work, it will be undefined
 
-// length property
-console.log(myArray.length);
+console.log(anotherObj["alive"]);
+console.log(anotherObj["beverage"]); // for nested object
 
-// last element in an array
-console.log(myArray[myArray.length - 1]);
-
-console.log(myArray[1]);
-
-// add more data to our array
-myArray.push("school");
-
-console.log(myArray);
-
-// remove something from our array
-const lastItem = myArray.pop();
-console.log(lastItem);
-
-// add and remove something from front
-const newLength = myArray.unshift(42);
-
-console.log(newLength);
-console.log(myArray);
-
-// remove
-const firstItem = myArray.shift();
-
-console.log(firstItem);
-console.log(myArray);
-
-// remove or replace something from the midel of the array by 'splice' methods
-
-// replace
-myArray.splice(1, 1, 42);
-console.log(myArray);
-
-// remove
-myArray.splice(1, 1);
-console.log(myArray);
-
-// not delete but incert a value or element in the midel of the array
-myArray.splice(1, 0, 1001);
-console.log(myArray);
-
-// Different type of methods
-const myArray_2 = ["A", "B", "C", "D", "E", "F"];
-
-const newArray = myArray_2.slice(2, 5);
-
-console.log(newArray);
-console.log(myArray_2);
-
-// reverse methods
-const newArray_2 = myArray_2.reverse();
-console.log(newArray_2); 
-
-// join methods
-const newArray_3 = myArray_2.join(); // it creates string
-const newString = newArray_3;
-console.log(newArray_3);
-
-// split methods
-const newArray_4 = newString.split(",");
-console.log(newArray_4);
-
-// concat methods
-const myArrayA = ["a", "b", "c"];
-const myArrayB = ["d", "e", "f"];
-
-const newArray_5 = myArrayB.concat(myArrayA);
-console.log(newArray_5);
-
-// spred operator [...]
-const newArray_6 = [...myArrayA, ...myArrayB];
-console.log(newArray_6);
-// if not spred operator
-const newArray_7 = [myArrayA, myArrayB];
-console.log(newArray_7);
-
-// multiple array
-const equipShelfA = ["baseball", "football", "volleyball"];
-const equipShelfB = ["basketball", "golf balls", "tennis balls"];
-
-const clothesShelfA = ["tank tops", "t-shirts", "jerseys"];
-const clothesShelfB = ["sweat tops", "sweat pants", "hoodies"];
-
-console.log(equipShelfA[1]);
-//or
-console.log(clothesShelfB[0]);
-
-const equipDept = [equipShelfA, equipShelfB];
-const clothesDept = [clothesShelfA, clothesShelfB];
-
-console.log(equipDept[0][1]);
-console.log(clothesDept[1][0]);
-
-// Three dimensional array
-const sportsStore = [equipDept, clothesDept];
-console.log(sportsStore);
-console.log(sportsStore[0][0][1]);
-console.log(sportsStore[1][1][0]);
+console.log(anotherObj.beverage.morning); // for nested object
