3ecaf5a7d62ecaae5a37509240532389eab513e4
masrik-dev <111740924+masrik-dev@users.noreply.github.com>
2026-03-28 23:41:51 +0600

    --update class

--- JS2/js/test.js
+++ JS2/js/test.js
@@ -1,91 +1,31 @@
-// Objects
-// key-value pairs in curly braces
-const myObj = { name: "Dove" };
-console.log(myObj.name);
-
-const anotherObj = {
-    alive: true,
-    answer: 42,
-    hobbies: ["Eat", "Sleep", "Code"],
-    beverage: {
-        morning: "Coffee",
-        afternoor: "Iced Tea"
-    },
-    action: function() {
-        return `Time for ${this.beverage.morning}`;
-    }
-};
-
-console.log(anotherObj.answer);
-console.log(anotherObj.hobbies[1]);
-console.log(anotherObj.beverage[0]); // won't work, it will be undefined
-
-console.log(anotherObj["alive"]);
-console.log(anotherObj["beverage"]); // for nested object
-
-console.log(anotherObj.beverage.morning); // for nested object
-
-console.log(anotherObj.action());  // action is method
-
-// New Object
-
-const vehicle = {
-    wheels: 4,
-    engine: function() {
-        return "Vrrooom!";
-    }
-}
-
-const truck = Object.create(vehicle);
-truck.doors = 2;
-console.log(truck);
-console.log(truck.wheels);  // Inheritance
-console.log(truck.engine());
-
-const car = Object.create(vehicle);
-car.doors = 4;
-car.engine = function() {
-    return "Whooooo!";
-};
-console.log(car.engine());
-console.log(car.wheels);
-
-const tesla = Object.create(car);
-console.log(tesla.wheels);
-tesla.engine = function() {
-    return "Shhhhh....";
-};
-console.log(tesla.engine());
-
-// Let's create a Band Object
-const band = {
-    vocals: "Robert Plant",
-    guitar: "Jimmy Page",
-    bass: "John Paul Jones",
-    drums: "John Bonham"
-};
-
-
-console.log(Object.keys(band));
-console.log(Object.values(band));
-// for delete a property
-delete band.drums;
-console.log(band.hasOwnProperty("drums"));
-console.log(Object.keys(band));
-console.log(Object.values(band));
-
-for (let job in band) {
-    console.log(band[job]);  // for each of the object. if it is single object then band.X
-    console.log(`On ${job}, it's ${band[job]}!`);
-}
-
-
-// destructuring objects
-const { guitar: myVariable, bass: myBass } = band;
-console.log(myVariable);
-console.log(myBass);
-
-function sings({ vocals }) { return `${vocals} sings!` };
-console.log(sings(band));
-
 // Javascript Classes
+class Pizza {
+    constructor(pizzaType, pizzaSize) {
+        this.type = pizzaType;
+        this.size = pizzaSize;
+        this.crust = "original";
+        this.toppings = [];
+    }
+    getCrust() {
+        return this.crust;
+    }
+    setCrust(pizzaCrust) {
+        this.crust = pizzaCrust;
+    }
+    getToppings() {
+        return this.toppings;
+    }
+    setToppings(topping){
+        this.toppings.push(topping);
+    }
+    bake() {
+        console.log(`Baking a ${this.size} ${this.type} ${this.crust} crust pizza.`);
+    }
+}
+
+const myPizza = new Pizza("pepperoni", "small");
+myPizza.setCrust("thin");
+myPizza.bake();
+myPizza.setToppings("sausage");
+myPizza.setToppings("olives");
+console.log(myPizza.getToppings());
