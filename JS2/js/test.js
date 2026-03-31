9ae597495f3bf2a2bbabe01cce0a9cb1e1cd1e2c
masrik-dev <111740924+masrik-dev@users.noreply.github.com>
2026-03-31 08:35:29 +0600

    --update JSON_1

--- JS2/js/test.js
+++ JS2/js/test.js
@@ -1,28 +1,7 @@
-// Javascript Classes
-//privet and public field
-
-class Pizza {
-    crust = "original";      //public field
-    #sauce = "traditional";  //privet field
-    #size;
-    constructor(pizzaSize) {
-        this.#size = pizzaSize;
-    }
-    getCrust() {
-        return this.crust;
-    }
-    setCrust(pizzaCrust) {
-        this.crust = pizzaCrust;
-    }
-    hereYouGo() {
-        console.log(`Here's your ${this.crust} ${this.#sauce} sauce ${this.#size} pizza.`);
-    }
-}
-
-const myPizza = new Pizza("large");
-myPizza.hereYouGo();
-console.log(myPizza.getCrust()); //public field
-console.log(myPizza.sauce);     //privet field
-
-
-
+//JSON: JavaScript Object Notation
+/*
+JSON is used to send and receive data.
+JSON is a text format that is completely language independent.
+Meaning JSON is used to send & receive data in many languages
+...not just in JaveScript.
+*/
