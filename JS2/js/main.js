a6e2dc6eb6d7d0a66a8b12c9e6d8df5d7a6fcd14
masrik-dev <111740924+masrik-dev@users.noreply.github.com>
2026-03-31 23:51:45 +0600

    --update JSON

--- JS2/js/main.js
+++ JS2/js/main.js
@@ -864,3 +864,33 @@ const myPizza = new Pizza("large");
 myPizza.hereYouGo();
 console.log(myPizza.getCrust()); //public field
 console.log(myPizza.sauce);     //privet field
+
+
+//JSON: JavaScript Object Notation
+/*
+JSON is used to send and receive data.
+JSON is a text format that is completely language independent.
+Meaning JSON is used to send & receive data in many languages
+...not just in JaveScript.
+*/
+
+const myObj = {
+    name: "Dave",
+    hobbies: ["eat", "sleep", "code"],
+    hello: function () {
+        console.log("Hello!");
+    }
+};
+console.log(myObj);
+console.log(myObj.name);
+myObj.hello();
+console.log(typeof myObj);
+
+const sendJSON = JSON.stringify(myObj);
+console.log(sendJSON);
+console.log(typeof sendJSON);
+console.log(sendJSON.name);
+
+const receiveJSON = JSON.parse(sendJSON);
+console.log(receiveJSON);
+console.log(typeof receiveJSON);
