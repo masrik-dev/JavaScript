2493844a8aa79bde27af6c86a1e9e39a55096743
masrik-dev <111740924+masrik-dev@users.noreply.github.com>
2026-04-04 23:40:55 +0600

    --update DOM_last

--- JS2/js/main.js
+++ JS2/js/main.js
@@ -951,3 +951,23 @@ view2.style.flexDirection = "row";
 view2.style.flexWrap = "wrap";
 view2.style.margin = "10px";
 
+while (view2.lastChild) {
+    view2.lastChild.remove();
+}
+
+const createDivs = (parent, iter) => {
+    const newDiv = document.createElement("div");
+    newDiv.textContent = iter;
+    newDiv.style.backgroundColor = "#000";
+    newDiv.style.width = "100px";
+    newDiv.style.height = "100px";
+    newDiv.style.margin = "10px";
+    newDiv.style.display = "flex";
+    newDiv.style.justifyContent = "center";
+    newDiv.style.alignItems = "center";
+    parent.append(newDiv);
+};
+// createDivs(view2, 10);
+for (let i = 1; i <= 12; i++) {
+    createDivs(view2, i);
+}
