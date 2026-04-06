fd07c275dc041b08d3f79e893789ccf0efb3ee0f
masrik-dev <111740924+masrik-dev@users.noreply.github.com>
2026-04-06 23:55:12 +0600

    --update event listeners

--- JS2/js/test.js
+++ JS2/js/test.js
@@ -1,1 +1,13 @@
 // JavaScript Event Listeners
+const view = document.querySelector("#view2");
+const div = view.querySelector("div");
+const h2 = div.querySelector("h2");
+
+// Syntax: addEventListener(event, function, useCapture)
+
+const doSomething = () => {
+    alert("doing something");
+}
+
+h2.addEventListener("click", doSomething, false);
+
