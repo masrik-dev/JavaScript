61ff60ad2868c0d0cddc56fde4ea056e882251bd
masrik-dev <111740924+masrik-dev@users.noreply.github.com>
2026-04-07 00:45:27 +0600

    --update event_listener_3

--- JS2/js/main.js
+++ JS2/js/main.js
@@ -971,3 +971,54 @@ const createDivs = (parent, iter) => {
 for (let i = 1; i <= 12; i++) {
     createDivs(view2, i);
 }
+
+
+// JavaScript Event Listeners
+const view = document.querySelector("#view2");
+const div = view.querySelector("div");
+const h2 = div.querySelector("h2");
+
+// Syntax: addEventListener(event, function, useCapture)
+
+const doSomething = () => {
+    alert("doing something");
+}
+
+h2.addEventListener("click", doSomething, false);
+h2.removeEventListener("click", doSomething, false);
+
+h2.addEventListener("click", (event) => {
+    console.log(event.target);
+    event.target.textContent = "Clicked";
+})
+
+
+// use of propagation
+
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
+        console.log("readyState: complete");
+        initApp();
+    }
+});
+
+const initApp = () => {
+    const view = document.querySelector("#view2");
+    const div = view.querySelector("div");
+    const h2 = div.querySelector("h2");
+
+    view.addEventListener("click", (event) => {
+        view.style.backgroundColor = "purple";
+    }, false);
+
+    div.addEventListener("click", (event) => {
+    div.style.backgroundColor = "blue";
+    }, false);
+
+    h2.addEventListener("click", (event) => {
+        event.stopPropagation();
+        event.target.textContent = "Clicked";
+    }, false);
+};
+
+// 5.00.00
--- JS2/js/test.js
+++ JS2/js/test.js
@@ -1,13 +1,34 @@
-// JavaScript Event Listeners
+// JavaScript Event Listeners 
 const view = document.querySelector("#view2");
 const div = view.querySelector("div");
 const h2 = div.querySelector("h2");
 
 // Syntax: addEventListener(event, function, useCapture)
 
-const doSomething = () => {
-    alert("doing something");
-}
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
+        console.log("readyState: complete");
+        initApp();
+    }
+});
 
-h2.addEventListener("click", doSomething, false);
+const initApp = () => {
+    const view = document.querySelector("#view2");
+    const div = view.querySelector("div");
+    const h2 = div.querySelector("h2");
 
+    view.addEventListener("click", (event) => {
+        view.style.backgroundColor = "purple";
+    }, false);
+
+    div.addEventListener("click", (event) => {
+    div.style.backgroundColor = "blue";
+    }, false);
+
+    h2.addEventListener("click", (event) => {
+        event.stopPropagation();
+        event.target.textContent = "Clicked";
+    }, false);
+};
+
+// 5.00.00
